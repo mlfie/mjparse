c30ee4773531c4fb71e25abdc36fe97f7303c628
fetaro <fetaro@gmail.com>
2011-05-14 06:46:14 +0000

    agari.rbを復活

--- rails/lib/mjt/analysis/pai.rb
+++ rails/lib/mjt/analysis/pai.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-module Mjt::Analysis
+module Mjt
+ module Analysis
   class Pai
     attr_accessor :type,    # 牌の種類(m:萬子 s:索子 p:筒子 j:字牌)
                   :number,  # 数字(字牌の場合、1:東 2:南 3:西 4:北 5:白 6:發 7:中)
@@ -25,6 +26,6 @@ module Mjt::Analysis
     def chuchan?
       return ! yaochu?
     end
-    
-  end
+  end   
+ end
 end
