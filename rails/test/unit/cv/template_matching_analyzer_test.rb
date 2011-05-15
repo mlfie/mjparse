606d00987b90d9b7564ff5b66cebd0785bb68483
gino <miragino@gmail.com>
2011-05-15 07:56:28 +0000

    テストケースを一般的な画像へ変更

--- rails/test/unit/cv/template_matching_analyzer_test.rb
+++ rails/test/unit/cv/template_matching_analyzer_test.rb
@@ -19,7 +19,7 @@ class TemplateMatchingAnalyzerTest < ActiveSupport::TestCase
       @debug = true
     end
     win = GUI::Window.new "result"
-    path = "public/img/3.jpg"
+    path = "lib/cv/test_img/test004.jpg"
     img = IplImage.load(path, CV_LOAD_IMAGE_GRAYSCALE)
     pais = @tma.analyze_raw(path)
     pais.each do |pai|
