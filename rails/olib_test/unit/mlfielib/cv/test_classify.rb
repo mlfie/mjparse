4096e048ca7821a71a442de72760f348a889e55e
gino <miragino@gmail.com>
2011-06-05 09:36:50 +0000

    refs #158 lib/cvのテストをolib_test配下へ移動

--- rails/olib_test/unit/mlfielib/cv/test_classify.rb
+++ rails/olib_test/unit/mlfielib/cv/test_classify.rb
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 require 'cv/template_matching_classifier'
 
-DIRPATH = 'lib/cv/test_img/'
+DIRPATH = '/test_img/'
 IMGNAME = 'test004.jpg'
 
 tmc = CV::TemplateMatchingClassifier.new
 
-pai_list = tmc.classify(DIRPATH + IMGNAME)
+pai_list = tmc.classify(File.dirname(__FILE__) + DIRPATH + IMGNAME)
 p pai_list.size
 #puts pai_list[0].type
 
