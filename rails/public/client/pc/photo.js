d3892db34413bd4d50fc149bb6fab7f1b13e7ab3
fetaro <fetaro@gmail.com>
2011-06-19 07:23:29 +0000

    ブラウザから画像を投稿できるようにした

--- rails/public/client/pc/photo.js
+++ rails/public/client/pc/photo.js
@@ -1,43 +1,12 @@
-var pictureSource;   // 写真ソース
-var destinationType; // 戻り値のフォーマット
 
-var base64str="";
-
-// PhoneGapがデバイスと接続するまで待機
-//
 function onLoad() {
 }
 
-// PhoneGap準備完了
-//
-function onDeviceReady() {
-}
-
 function capturePhoto() {
-  window.open('pc/take_photo.html','a','width=300,menubar=no,toolbar=no'); 
+  window.open('pc/post_file.html','a','menubar=no,toolbar=no'); 
 }
 
 function selectPhoto() {
-  window.open('pc/take_photo.html','a','width=300,menubar=no,toolbar=no'); 
+  window.open('pc/post_file.html','a','menubar=no,toolbar=no'); 
 }
 
-// 写真の撮影に失敗した場合
-function onFail(mesage) {
-  alert('エラーが発生しました: ' + message);
-}
-
-// 写真の撮影に成功した場合
-function onPhotoDataSuccess(imageData) {
-  // 下記のコメントを外すことでBase64形式のデータをログに出力
-  //console.log(imageData);
-  console.log("scucess to take a photo. Let's send");
-  infomsg("scucess to take a photo. Let's send")
-
-  base64str = imageData;
-
-  // 画像表示
-  var smallImage = document.getElementById('image');
-  smallImage.style.display = 'block';
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-
-}
