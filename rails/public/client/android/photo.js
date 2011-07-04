bd4e8d9cb047860744795037cfcf538791cd6294
fetaro <fetaro@gmail.com>
2011-07-04 15:48:08 +0000

    不要ファイル削除など

---
+++ rails/public/client/android/photo.js
@@ -0,0 +1,11 @@
+
+function capturePhoto(params) {
+  //androidlog.d("capture button pressed");
+  androidcamera.capture(params);
+}
+
+function selectPhoto(params) {
+  //androidlog.d("select button pressed");
+  androidcamera.select(params);
+}
+
