a946bab63b3a286320a62f71967e8dcf365910b1
fetaro <fetaro@gmail.com>
2011-09-24 03:19:07 +0000

    整理中

---
+++ client/platform/android/src/biz/fedc/mjt/client/android/App.java
@@ -0,0 +1,13 @@
+package biz.fedc.mjt.client.android;
+
+
+import android.os.Bundle;
+import com.phonegap.*;
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
