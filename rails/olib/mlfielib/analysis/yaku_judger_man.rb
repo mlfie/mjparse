b0d77e1374bc2159d7730296c27dbc1bb322903f
lassy <lassy423@gmail.com>
2011-11-03 08:12:34 +0000

    役満の判定メソッドをインスタンスメソッド化

--- rails/olib/mlfielib/analysis/yaku_judger_man.rb
+++ rails/olib/mlfielib/analysis/yaku_judger_man.rb
@@ -8,12 +8,12 @@ module Mlfielib
     class YakuJudger
 
       # 国士無双
-      def self.kokushi?(tehai, agari)
+      def kokushi?(tehai, agari)
         return false
       end
 
       # 四暗刻
-      def self.suankou?(tehai, agari)
+      def suankou?(tehai, agari)
         tehai.mentsu_list.each do |mentsu|
           if mentsu.mentsu_type != 'k' 
             return false
@@ -25,7 +25,7 @@ module Mlfielib
       end
 	  
       # 大三元
-      def self.daisangen?(tehai, agari)
+      def daisangen?(tehai, agari)
         has_haku = false
         has_chun = false
         has_hatsu = false
@@ -47,57 +47,57 @@ module Mlfielib
         end
         return false
       end
-  	
-	  def self.sukantsu?(tehai, agari)
-	    count = 0
-	    tehai.mentsu_list.each do | mentsu|
+
+      def sukantsu?(tehai, agari)
+        count = 0
+        tehai.mentsu_list.each do | mentsu|
           if mentsu.mentsu_type == "4"
-		    count += 1
-		  end
-		end
-		if count > 3
-		  return true
-		end
-	    return false
+		        count += 1
+		      end
+        end
+		    if count > 3
+		      return true
+		    end
+        return false
       end
 	  
-      def self.tenho?(tehai, agari)
-	    if agari.is_tenho
-		  return true
-	    end
-		return false
+      def tenho?(tehai, agari)
+        if agari.is_tenho
+          return true
+        end
+		    return false
       end
 	  
-      def self.chiho?(tehai, agari)
-	    if agari.is_chiho
-		  return true
-	    end
-		return false
+      def chiho?(tehai, agari)
+        if agari.is_chiho
+          return true
+        end
+		    return false
       end	   
 	  
-      def self.tasushi?(tehai, agari)
-	    tehai.mentsu_list.each do | mentsu|
+      def tasushi?(tehai, agari)
+        tehai.mentsu_list.each do | mentsu|
           if mentsu.mentsu_type == "k" && mentsu.pai_list[0].type == "j" && mentsu.pai_list[0].number == "1"
             tehai.mentsu_list.each do | mentsu2|
               if mentsu2.mentsu_type == "k" && mentsu2.pai_list[0].type == "j" && mentsu2.pai_list[0].number == "2"
                 tehai.mentsu_list.each do | mentsu3|
                   if mentsu3.mentsu_type == "k" && mentsu3.pai_list[0].type == "j" && mentsu3.pai_list[0].number == "3"
-					tehai.mentsu_list.each do | mentsu4|
-					  if mentsu4.mentsu_type == "k" && mentsu4.pai_list[0].type == "j" && mentsu4.pai_list[0].number == "4"
+                    tehai.mentsu_list.each do | mentsu4|
+                      if mentsu4.mentsu_type == "k" && mentsu4.pai_list[0].type == "j" && mentsu4.pai_list[0].number == "4"
                         return true
                       end
                     end
-				  end	
+				          end	
                 end
-			  end
+              end
             end
           end
-		end  
-		return false
+        end  
+		    return false
       end	  
 	  
-      def self.shosushi?(tehai, agari)
-	    tehai.mentsu_list.each do | mentsu |
+      def shosushi?(tehai, agari)
+        tehai.mentsu_list.each do | mentsu |
           if mentsu.mentsu_type != "t" && mentsu.pai_list[0].type != "j" 
             tehai.mentsu_list.each do | mentsu2 |		  
               if mentsu2.pai_list[0].type == "j" && mentsu2.pai_list[0].number == "1"
@@ -105,46 +105,46 @@ module Mlfielib
                   if mentsu3.pai_list[0].type == "j" && mentsu3.pai_list[0].number == "2"
                     tehai.mentsu_list.each do | mentsu4 |
                       if mentsu4.pai_list[0].type == "j" && mentsu4.pai_list[0].number == "3"
-					    tehai.mentsu_list.each do | mentsu5 |
-					      if mentsu5.pai_list[0].type == "j" && mentsu5.pai_list[0].number == "4"
+                        tehai.mentsu_list.each do | mentsu5 |
+                          if mentsu5.pai_list[0].type == "j" && mentsu5.pai_list[0].number == "4"
                             return true
                           end
                         end
-					  end
-				    end	
+                      end
+                    end	
                   end
-			    end
+                end
               end
             end
-		  end  
-	   end 
-	   return false
+          end  
+        end 
+        return false
       end		  
 	  	  
-      def self.tsuiso?(tehai, agari)
-	    tehai.mentsu_list.each do | mentsu |	  
-	      if mentsu.pai_list[0].type != "j"
-			return false
-		  end
-	    end
-		return true
-	  end
+      def tsuiso?(tehai, agari)
+        tehai.mentsu_list.each do | mentsu |
+          if mentsu.pai_list[0].type != "j"
+            return false
+		      end
+		    end
+        return true
+      end
 	  
-      def self.chinraoto?(tehai, agari)
-	    tehai.mentsu_list.each do | mentsu |	  
-	      if mentsu.pai_list[0].type != "j"
-		    if mentsu.mentsu_type != "k" && mentsu.mentsu_type != "t" 
-			  return false
-			elsif mentsu.pai_list[0].type != "1" || mentsu.pai_list[0].type != "9"
-			    return false
+      def chinraoto?(tehai, agari)
+        tehai.mentsu_list.each do | mentsu |
+          if mentsu.pai_list[0].type != "j"
+            if mentsu.mentsu_type != "k" && mentsu.mentsu_type != "t"
+              return false
+            elsif mentsu.pai_list[0].type != "1" || mentsu.pai_list[0].type != "9"
+              return false
 			end
 	      end
 	    end
 		return true	  
 	  end
 	  
-      def self.ryuiso?(tehai, agari); return false; end 
-      def self.churen?(tehai, agari); return false; end
+      def ryuiso?(tehai, agari); return false; end 
+      def churen?(tehai, agari); return false; end
 	  
 	  
 	  
