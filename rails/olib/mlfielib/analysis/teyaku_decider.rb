944ee31596ef33317fa6e62100303f3ed36886b0
fetaro <fetaro@gmail.com>
2011-11-09 12:56:52 +0000

    TeyakuDeciderにログ出力追加

--- rails/olib/mlfielib/analysis/teyaku_decider.rb
+++ rails/olib/mlfielib/analysis/teyaku_decider.rb
@@ -57,6 +57,10 @@ module Mlfielib
               when YakuJudger::RESULT_SUCCESS then
                 tehai.yaku_list = judger.yaku_list
                 yaku_established = true
+		STDERR.puts "判定した結果、以下の役が得られました。"
+		tehai.yaku_list.each do |yaku|
+                  STDERR.puts yaku.name + ":" + yaku.kanji
+                end 
                 # 得点を計算する
                 tehai = ScoreCalculator.calculate_point(tehai, kyoku)
               when YakuJudger::RESULT_ERROR_YAKUNASHI then
