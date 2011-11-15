f64b8c29c2476731a3fbe0d2da9e733771c3a6ac
fetaro <fetaro@gmail.com>
2011-11-15 01:25:19 +0000

    statusコードが200かどうか判定するロジックを追加

--- client/src/common/www/lib/point.js
+++ client/src/common/www/lib/point.js
@@ -17,51 +17,58 @@ var Point = function(agari){
     this.toHtml = function () {
         var html = "";
         var doraCount =0;
-        html += "<table>";
-        $.each(this.agari.yaku_list, function () {
-                   if(this.name == "dora"){
-                       doraCount += 1;
-                   }else{ 
-                       html += "<tr>"
-                           + "<td>" + this.name_kanji + "<\/td>"
-                           + "<td>" + this.han_num + "飜<\/td>"
-                           + "<\/tr>";
-                   }
-               });
-        //ドラはまとめる
-        if(doraCount != 0){
-            html += "<tr>"
-                + "<td>ドラ" + doraCount + "<\/td>"
-                + "<td>" + doraCount + "飜<\/td>"
-                + "<\/tr>";
-        }     
 
-        html += "<\/table>";
+        if(this.agari.status_code == 200){
+            
+            html += "<table>";
+            $.each(this.agari.yaku_list, function () {
+                       if(this.name == "dora"){
+                           doraCount += 1;
+                       }else{ 
+                           html += "<tr>"
+                               + "<td>" + this.name_kanji + "<\/td>"
+                               + "<td>" + this.han_num + "飜<\/td>"
+                               + "<\/tr>";
+                       }
+                   });
+            //ドラはまとめる
+            if(doraCount != 0){
+                html += "<tr>"
+                    + "<td>ドラ" + doraCount + "<\/td>"
+                    + "<td>" + doraCount + "飜<\/td>"
+                    + "<\/tr>";
+            }     
 
-        html += "<p>";
-        html += this.agari.total_fu_num + "符"
-            + this.agari.total_han_num + "飜";
-        html += "</p>";
+            html += "<\/table>";
 
-        if(this.agari.mangan_scale != 0){
             html += "<p>";
-            html += Point.MANGANSTR[this.agari.mangan_scale];
-            html += "</p>";            
-        }
-        
-        if (this.agari.is_tsumo){
-            if (this.agari.is_parent) {
-                html += this.agari.child_point + "点オール";
-            } else {
-                html += "子" + this.agari.child_point + "点/"
-                    + "親" + this.agari.parent_point + "点";
-            }
-        }else{
-            html += this.agari.total_point + "点";
-        }
+            html += this.agari.total_fu_num + "符"
+                + this.agari.total_han_num + "飜";
+            html += "</p>";
 
-        dbgmsg("Point.toHtml",html);
-        return html;
+            if(this.agari.mangan_scale != 0){
+                html += "<p>";
+                html += Point.MANGANSTR[this.agari.mangan_scale];
+                html += "</p>";            
+            }
+            
+            if (this.agari.is_tsumo){
+                if (this.agari.is_parent) {
+                    html += this.agari.child_point + "点オール";
+                } else {
+                    html += "子" + this.agari.child_point + "点/"
+                        + "親" + this.agari.parent_point + "点";
+                }
+            }else{
+                html += this.agari.total_point + "点";
+            }
+
+            dbgmsg("Point.toHtml",html);
+            return html;
+
+        }else{
+            return "<b>画像解析に失敗しました。</b><br>写真を変えるか、解析結果を訂正して再度得点計算してください。";
+        }
     };
 };
 
