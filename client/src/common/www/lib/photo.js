af5338290aea3d184c1159c90b93b27024eca279
fetaro <fetaro@gmail.com>
2011-11-22 14:49:11 +0000

    写真撮影した後に、サムネイルが正方形になってしまうバグを修正

--- client/src/common/www/lib/photo.js
+++ client/src/common/www/lib/photo.js
@@ -2,41 +2,41 @@ var Photo = function(){
     var NO_IMAGE="img/nophoto.jpg";//「手牌画像なし」と書いた画像
     var IMG_URL = "#img_url";
     
-    this.imgUrl = $("#img_url");
-    this.topPhoto = $("#img_top_photo");
-    this.resultPhoto = $("#img_result_photo");
+    this.jqImgUrl = $("#img_url");
+    this.jqTopPhoto = $("#img_top_photo");
+    this.jqResultPhoto = $("#img_result_photo");
     this.base64 ="";
 
     this.clear = function(){
-        this.imgUrl.val("");
-        this.topPhoto.attr("src" , NO_IMAGE);
-        this.resultPhoto.attr("src", NO_IMAGE);
+        this.jqImgUrl.val("");
+        this.jqTopPhoto.attr("src" , NO_IMAGE);
+        this.jqResultPhoto.attr("src", NO_IMAGE);
         this.base64 ="";
     };
 
     this.setUrlSrc = function(url,thumUrl){
         dbgmsg("Photo.setUrlSrc",url + "," +thumUrl);
-        this.imgUrl.val(url);
-        this.topPhoto.attr("src" , thumUrl);
-        this.resultPhoto.attr("src", thumUrl);
+        this.jqImgUrl.val(url);
+        this.jqTopPhoto.attr("src" , thumUrl);
+        this.jqResultPhoto.attr("src", thumUrl);
     };
 
     this.setBase64Src = function(base64){
         dbgmsg("Photo.setBase64Src",base64);
-        this.imgUrl.val(""); 
-        this.topPhoto.attr("src" , "data:image/jpeg;base64," + base64);
-        this.resultPhoto.attr("src",  "data:image/jpeg;base64," + base64);
+        this.jqImgUrl.val(""); 
+        this.jqTopPhoto.attr("src" , "data:image/jpeg;base64," + base64);
+        this.jqResultPhoto.attr("src",  "data:image/jpeg;base64," + base64);
         this.base64 = base64;
     };
 
     this.setUrl = function(url){
         dbgmsg("Photo.setUrl",url);
-        this.imgUrl.val(url);
+        this.jqImgUrl.val(url);
     };
 
     this.getUrl = function(){
-        dbgmsg("Photo.getUrl", this.imgUrl.val());
-        return this.imgUrl.val();
+        dbgmsg("Photo.getUrl", this.jqImgUrl.val());
+        return this.jqImgUrl.val();
     };
 
     this.getBase64 = function(){
@@ -44,14 +44,14 @@ var Photo = function(){
     };
 
     this.isLocalPhoto = function(){
-        return this.imgUrl.val() == "" && this.base64 !="";
+        return this.jqImgUrl.val() == "" && this.base64 !="";
     };
 
     this.isEmpty = function(){
-        return this.imgUrl.val() == "" && this.base64 =="";
+        return this.jqImgUrl.val() == "" && this.base64 =="";
     };
 
     this.isServerPhoto = function(){
-        return this.imgUrl.val() != "";
+        return this.jqImgUrl.val() != "";
     };
 };
