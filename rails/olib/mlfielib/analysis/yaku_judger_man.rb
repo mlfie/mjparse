1f743d69b77c71a0fc57422ad6e2e6b3c3eb9b93
syokenz <syokenz@gmail.com>
2011-11-23 08:07:21 +0000

    国士無双を実装、テスト

--- rails/olib/mlfielib/analysis/yaku_judger_man.rb
+++ rails/olib/mlfielib/analysis/yaku_judger_man.rb
@@ -9,6 +9,169 @@ module Mlfielib
 
       # 国士無双
       def kokushi?(tehai, agari)
+        # 頭とメンツで公九牌が13種類以上あること
+        count = 0
+        # m1
+        if tehai.atama.number == 1 && tehai.atama.type == Pai::PAI_TYPE_MANZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list do |pai|
+              if pai.number == 1 && pai.type == Pai::PAI_TYPE_MANZU
+                count += 1
+              end
+            end
+          end
+        end
+        # m9
+        if tehai.atama.number == 9 && tehai.atama.type == Pai::PAI_TYPE_MANZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == 9 && pai.type == Pai::PAI_TYPE_MANZU
+                count += 1
+              end
+            end
+          end
+        end
+        # s1
+        if tehai.atama.number == 1 && tehai.atama.type == Pai::PAI_TYPE_SOUZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == 1 && pai.type == Pai::PAI_TYPE_SOUZU
+                count += 1
+              end
+            end
+          end
+        end
+        # s9
+        if tehai.atama.number == 9 && tehai.atama.type == Pai::PAI_TYPE_SOUZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == 9 && pai.type == Pai::PAI_TYPE_SOUZU
+                count += 1
+              end
+            end
+          end
+        end
+        # p1
+        if tehai.atama.number == 1 && tehai.atama.type == Pai::PAI_TYPE_PINZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == 1 && pai.type == Pai::PAI_TYPE_PINZU
+                count += 1
+              end
+            end
+          end
+        end
+        # p9
+        if tehai.atama.number == 9 && tehai.atama.type == Pai::PAI_TYPE_PINZU
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == 9 && pai.type == Pai::PAI_TYPE_PINZU
+                count += 1
+              end
+            end
+          end
+        end
+        # 東
+        if tehai.atama.number == Pai::PAI_NUMBER_TON && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_TON && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 南
+        if tehai.atama.number == Pai::PAI_NUMBER_NAN && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_NAN && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 西
+        if tehai.atama.number == Pai::PAI_NUMBER_SHA && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_SHA && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 北
+        if tehai.atama.number == Pai::PAI_NUMBER_PEI && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_PEI && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 白
+        if tehai.atama.number == Pai::PAI_NUMBER_HAKU && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_HAKU && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 発
+        if tehai.atama.number == Pai::PAI_NUMBER_HATSU && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_HATSU && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+        # 中
+        if tehai.atama.number == Pai::PAI_NUMBER_CHUN && tehai.atama.type == Pai::PAI_TYPE_JIHAI
+          count += 1
+        else
+          tehai.mentsu_list.each do |mentsu|
+            mentsu.pai_list.each do |pai|
+              if pai.number == Pai::PAI_NUMBER_CHUN && pai.type == Pai::PAI_TYPE_JIHAI
+                count += 1
+              end
+            end
+          end
+        end
+
+        if count >= 12
+          return true
+        end
+                
         return false
       end
 
@@ -47,7 +210,8 @@ module Mlfielib
         end
         return false
       end
-
+      
+      # 四槓子
       def sukantsu?(tehai, agari)
         count = 0
         tehai.mentsu_list.each do | mentsu|
@@ -61,20 +225,23 @@ module Mlfielib
         return false
       end
 	  
+	  # 天和
       def tenho?(tehai, agari)
         if agari.is_tenho
           return true
         end
-		    return false
+		  return false
       end
 	  
+	  # 地和
       def chiho?(tehai, agari)
         if agari.is_chiho
           return true
         end
-		    return false
+		  return false
       end	   
 	  
+	  # 大四喜
       def tasushi?(tehai, agari)
         tehai.mentsu_list.each do | mentsu|
           if mentsu.mentsu_type == "k" && mentsu.pai_list[0].type == "j" && mentsu.pai_list[0].number == "1"
@@ -96,6 +263,7 @@ module Mlfielib
 		    return false
       end	  
 	  
+	  # 小四喜
       def shosushi?(tehai, agari)
         tehai.mentsu_list.each do | mentsu |
           if mentsu.mentsu_type != "t" && mentsu.pai_list[0].type != "j" 
@@ -120,7 +288,8 @@ module Mlfielib
         end 
         return false
       end		  
-	  	  
+	  
+	  # 字一色	  
       def tsuiso?(tehai, agari)
         tehai.mentsu_list.each do | mentsu |
           if mentsu.pai_list[0].type != "j"
@@ -130,6 +299,7 @@ module Mlfielib
         return true
       end
 	  
+	  # 清老頭
       def chinraoto?(tehai, agari)
         tehai.mentsu_list.each do | mentsu |
           if mentsu.pai_list[0].type != "j"
@@ -143,7 +313,10 @@ module Mlfielib
 		return true	  
 	  end
 	  
+	  # 緑一色
       def ryuiso?(tehai, agari); return false; end 
+      
+      # 九蓮宝燈
       def churen?(tehai, agari); return false; end
 	  
 	  
---
+++ rails/olib_test/unit/mlfielib/analysis/yaku_judger_man_test.rb
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'test_helper'
+require 'mlfielib/analysis/pai'
+require 'mlfielib/analysis/mentsu'
+require 'mlfielib/analysis/tehai'
+require 'mlfielib/analysis/kyoku'
+require 'mlfielib/analysis/yaku_judger_man'
+require 'mlfielib/analysis/mentsu_resolver'
+
+class YakuJudgerManTest < Test::Unit::TestCase
+  #Constants
+  TON = Mlfielib::Analysis::Kyoku::KYOKU_KAZE_TON
+  NAN = Mlfielib::Analysis::Kyoku::KYOKU_KAZE_NAN
+  SHA = Mlfielib::Analysis::Kyoku::KYOKU_KAZE_SHA
+  PEI = Mlfielib::Analysis::Kyoku::KYOKU_KAZE_PEI
+  
+  def setup
+    yaku_specimen = Hash.new
+    # yaku_specimen[name] = Mlfielib::Analysis::YakuSpecimen.new(name, kanji, han_num, naki_han_num)
+    @judger = Mlfielib::Analysis::YakuJudger.new(yaku_specimen)
+    @resolver = Mlfielib::Analysis::MentsuResolver.new
+  end
+  
+  def teardown
+    
+  end
+  
+  def test_kokushi
+    # 13面待ち --> true
+    pai_items = "m1tm9tp1tp9ts1ts9tj1tj2tj3tj4tj5tj6tj7tm1t"    
+    @resolver.get_mentsu(pai_items)
+    
+    @resolver.tehai_list.each do |tehai|
+      assert_equal true, @judger.kokushi?(tehai, nil)
+    end
+    @resolver = Mlfielib::Analysis::MentsuResolver.new
+    
+    # 頭が東 --> true
+    pai_items = "m1tm9tp1tp9ts1ts9tj1tj1tj2tj3tj4tj5tj6tj7t"    
+    @resolver.get_mentsu(pai_items)
+    
+    @resolver.tehai_list.each do |tehai|
+      assert_equal true, @judger.kokushi?(tehai, nil)
+    end
+    @resolver = Mlfielib::Analysis::MentsuResolver.new
+    
+    # 頭が中帳牌 --> false
+    pai_items = "m1tm9tp1tp9ts1ts9tm5tm5tj2tj3tj4tj5tj6tj7t"    
+    @resolver.get_mentsu(pai_items)
+    
+    @resolver.tehai_list.each do |tehai|
+      assert_equal false, @judger.kokushi?(tehai, nil)
+    end
+    @resolver = Mlfielib::Analysis::MentsuResolver.new
+    
+    # 東が暗刻 --> false
+    pai_items = "m1tm9tp1tp9ts1ts9tj1tj1tj2tj3tj4tj5tj6tj1t"    
+    @resolver.get_mentsu(pai_items)
+    
+    @resolver.tehai_list.each do |tehai|
+      assert_equal false, @judger.kokushi?(tehai, nil)
+    end
+    @resolver = Mlfielib::Analysis::MentsuResolver.new
+  end
+  
+  
+#  def test_mentsure
+#    pai_items = "m3tm4ts2ts2ts3ts3ts4ts4tp9tp9tm2tj7rj7tj7t"    
+#    @resolver.get_mentsu(pai_items)
+#    
+#    @resolver.tehai_list.each do |tehai|
+#      tehai.mentsu_list.each do |mentsu|
+#        mentsu.pai_list.each do |pai|
+#          p pai
+#        end
+#      end
+#    end    
+#  end
+  
+end
